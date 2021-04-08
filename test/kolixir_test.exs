ff2b6e6ce5bac88e8690469345a6df01875dfb4e
Devan Kestel <devan.kestel@gmail.com>
2021-04-08 03:37:25 -0400

    Added wallaby, config, and first test.

--- test/kolixir_test.exs
+++ test/kolixir_test.exs
@@ -1,8 +1,21 @@
 defmodule KolixirTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Wallaby.Feature
   doctest Kolixir
+
+  import Wallaby.Query, only: [text_field: 1, button: 1, css: 2]
+
 
   test "greets the world" do
     assert Kolixir.hello() == :world
   end
+
+  feature "users can login", %{session: session} do
+    session
+    |> visit("/")
+    |> fill_in(text_field("loginname"), with: "MY_USERNAME")
+    |> fill_in(text_field("password"), with: "MY_PASSWORD")
+    |> click(button("Log In"))
+  end
+
 end
